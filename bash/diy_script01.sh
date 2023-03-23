2d1b38ee31639f7014dba92c51687aae676d6200
DinG <96579381+Joshua-DinG@users.noreply.github.com>
2023-03-23 12:29:56 +0800

    Update diy_script01.sh

--- bash/diy_script01.sh
+++ bash/diy_script01.sh
@@ -5,4 +5,4 @@
 sed -i '1i src-git haibo https://github.com/haiibo/openwrt-packages' feeds.conf.default
 
 # LINUX_VERSION
-sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(LINUX_VERSION)-/g' include/image.mk
+#sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(LINUX_VERSION)-/g' include/image.mk
